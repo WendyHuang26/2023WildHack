9c097d9253c25b45911e35d51a7c54833801a102
Wendy Huang <wenjinghuang26@gmail.com>
2023-04-16 12:40:06 -0500

    hardcore demo

--- navigation/screens/CollectionScreen.js
+++ navigation/screens/CollectionScreen.js
@@ -1,9 +1,14 @@
 import * as React from "react";
-import {View, ScrollView, StyleSheet, Text, Button, Dimensions, SafeAreaView, TouchableOpacity, StatusBar, font} from "react-native";
+import {View, Image, ScrollView, StyleSheet, Text, Button, Dimensions, SafeAreaView, TouchableOpacity, StatusBar, font} from "react-native";
+
+const dog = require('./dog.jpg');
+const willow = require('./willow.jpg');
+const daffodil = require('./daffodil.jpg');
 
 export default function CollectionScreen({ navigation }) {
     const [rectangles, setRectangles] = React.useState(1); // state to keep track of the number of rectangles
 
+    const count = 1;
     // function to add a new rectangle
     function addRectangle() {
         setRectangles((count) => count + 1); // increase the count of rectangles by 1
@@ -28,13 +33,35 @@ export default function CollectionScreen({ navigation }) {
             scrollEventThrottle={16} // set the throttle to 16ms to ensure that the onScroll event is called frequently enough
         >
             {[...Array(rectangles)].map((_, index) => (
-                <View style={styles.collection} key={index}></View>
+                <View style={styles.collection} key={index}>
+                    <View style={styles.verticalLine}></View>
+                </View>
             ))}
         </ScrollView>
 
         <TouchableOpacity onPress={addRectangle} style={styles.button}>
             <Text style={styles.buttonText}>Add Rectangle</Text>
         </TouchableOpacity>
+
+        <View style={styles.logoContainer}>
+            <Image source={dog} style={styles.logo} top={-160}></Image>
+        </View>
+
+        <View style={styles.logoContainer}>
+            <Image source={daffodil} style={styles.logo} top={-20}></Image>
+        </View>
+
+        <View style={styles.logoContainer}>
+            <Image source={willow} style={styles.logo} top={120}></Image>
+        </View>
+
+        <Text style = {styles.name} top = {110}>Husky Shephard</Text>
+        <Text style = {styles.name} top = {250}>Daffodil</Text>
+        <Text style = {styles.name} top = {390}>Willow</Text>
+
+        <Text style = {styles.name} top = {160}>Date: 04/16/2023</Text>
+        <Text style = {styles.name} top = {300}>Date: 04/16/2023</Text>
+        <Text style = {styles.name} top = {440}>Date: 04/16/2023</Text>
 
     </View>)
 }
@@ -76,15 +103,50 @@ const styles = StyleSheet.create({
         marginTop: 20,
         top: 50,
     },
+    verticalLine: {
+        shadowOffset: {width:0, height:0,},
+        shadowOpacity: 0.8,
+        shadowColor: 'grey',
+        shadowRadius: 2,
+        height: '80%',
+        opacity: 0.45,
+        width: 1,
+        marginTop: 10,
+        backgroundColor: 'grey',
+        alignSelf: 'center',
+    },
+    name: {
+        color: 'grey',
+        fontStyle: 'normal',
+        fontSize: 17,
+        lineHeight: 90,
+        textAlign: 'center',
+        position: "absolute",
+        left: 605,
+    },
 
     button: {
-        backgroundColor: "white",
+        backgroundColor: "transparent",
         padding: 10,
         borderRadius: 5,
     },
     buttonText: {
-    color: "black",
+    color: "transparent",
     fontWeight: "bold",
     fontSize: 16,
     },
+
+    logoContainer: {
+        position: 'absolute',
+        left: 0,
+        right: 160,
+        alignItems: 'center',
+        paddingTop: StatusBar.currentHeight || 0, // Add padding for devices with a status bar (e.g., Android)
+      },
+      logo: {
+        width: 100,
+        height: 100,
+        borderRadius:10,
+      },
+    
 })
--- navigation/screens/RankingScreen.js
+++ navigation/screens/RankingScreen.js
@@ -1,13 +1,60 @@
 import * as React from "react";
 import {View, ScrollView, StyleSheet, Text, Button, Dimensions, SafeAreaView, TouchableOpacity, StatusBar, font} from "react-native";
 
-export default function RankingScreen({ navigation }) {
+const dog = require('./dog.jpg');
+const willow = require('./willow.jpg');
+const daffodil = require('./daffodil.jpg');
+
+export default function CollectionScreen({ navigation }) {
+    const [rectangles, setRectangles] = React.useState(1); // state to keep track of the number of rectangles
+
+    const count = 1;
+    // function to add a new rectangle
+    function addRectangle() {
+        setRectangles((count) => count + 1); // increase the count of rectangles by 1
+    }
+
+    // event handler for onScroll event
+    function handleScroll(event) {
+        const { contentOffset } = event.nativeEvent;
+        if (contentOffset.y < 50) {
+            event.nativeEvent.contentOffset.y = 50;
+        }
+    }
+
     return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <View style={styles.container}>
         <Text>                                                                                                       </Text>
         <Text style={styles.titleText}>Ranking</Text>
 
-    </ScrollView>)
+        <ScrollView 
+            contentContainerStyle={styles.scrollContainer}
+            onScroll={handleScroll} // add onScroll event handler
+            scrollEventThrottle={16} // set the throttle to 16ms to ensure that the onScroll event is called frequently enough
+        >
+            {[...Array(rectangles)].map((_, index) => (
+                <View style={styles.collection} key={index}>
+                    <View style={styles.verticalLine1}></View>
+                    <View style={styles.verticalLine2}></View>
+                </View>
+            ))}
+        </ScrollView>
+
+        <TouchableOpacity onPress={addRectangle} style={styles.button}>
+            <Text style={styles.buttonText}>Add Rectangle</Text>
+        </TouchableOpacity>
+
+        <Text style = {styles.number1}>1</Text>
+        <Text style = {styles.number2}>2</Text>
+        <Text style = {styles.number3}>3</Text>
+        <Text style = {styles.score} top = {110}>9</Text>
+        <Text style = {styles.score} top = {200}>7</Text>
+        <Text style = {styles.score} top = {290}>3</Text>
+        <Text style = {styles.name} top = {110}>Wendy Huang</Text>
+        <Text style = {styles.name} top = {200}>Kefan Yu</Text>
+        <Text style = {styles.name} top = {290}>Tony Yang</Text>
+
+    </View>)
 }
 
 const styles = StyleSheet.create({
@@ -20,11 +67,115 @@ const styles = StyleSheet.create({
     titleText: {
         color: 'white',
         fontStyle: 'normal',
-        fontSize: 60,
+        fontSize: 52,
         lineHeight: 90,
         fontWeight: 'bold',
         textAlign: 'center',
         position: "absolute",
-        top: 100,
-    }
+        top: 25,
+    },
+    scrollContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+        top: 25,
+        paddingTop: 10, // add top padding to avoid content clipping
+        paddingBottom: 40,
+    },
+    collection: {
+        backgroundColor: 'white',
+        shadowOffset: {width:10, height:7,},
+        shadowOpacity: 0.2,
+        shadowColor: 'grey',
+        shadowRadius: 3,
+        width: 320,
+        height: 70,
+        borderRadius:20,
+        alignSelf: 'center',
+        marginTop: 20,
+        top: 50,
+    },
+    verticalLine1: {
+        shadowOffset: {width:0, height:0,},
+        shadowOpacity: 0.8,
+        shadowColor: 'grey',
+        shadowRadius: 2,
+        height: '70%',
+        opacity: 0.45,
+        width: 1,
+        marginTop: 10,
+        marginLeft: -180,
+        backgroundColor: 'grey',
+        alignSelf: 'center',
+    },
+    verticalLine2: {
+        shadowOffset: {width:0, height:0,},
+        shadowOpacity: 0.8,
+        shadowColor: 'grey',
+        shadowRadius: 2,
+        height: '70%',
+        opacity: 0.45,
+        width: 1,
+        marginTop: -50,
+        marginLeft: 180,
+        backgroundColor: 'grey',
+        alignSelf: 'center',
+    },
+    number1: {
+        color: 'grey',
+        fontStyle: 'normal',
+        fontSize: 40,
+        lineHeight: 90,
+        textAlign: 'center',
+        position: "absolute",
+        top: 110,
+        left: 455,
+    },
+    number2: {
+        color: 'grey',
+        fontStyle: 'normal',
+        fontSize: 40,
+        lineHeight: 90,
+        textAlign: 'center',
+        position: "absolute",
+        top: 200,
+        left: 455,
+    },
+    number3: {
+        color: 'grey',
+        fontStyle: 'normal',
+        fontSize: 40,
+        lineHeight: 90,
+        textAlign: 'center',
+        position: "absolute",
+        top: 290,
+        left: 455,
+    },
+    score: {
+        color: 'grey',
+        fontStyle: 'normal',
+        fontSize: 23,
+        lineHeight: 90,
+        textAlign: 'center',
+        position: "absolute",
+        left: 707,
+    },
+    name: {
+        color: 'grey',
+        fontStyle: 'normal',
+        fontSize: 26,
+        lineHeight: 90,
+        textAlign: 'center',
+        position: "absolute",
+    },
+
+    button: {
+        backgroundColor: "transparent",
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+    color: "transparent",
+    fontWeight: "bold",
+    fontSize: 16,
+    },
 })
